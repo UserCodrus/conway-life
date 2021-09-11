6c2f1b09069a5907634371bac300bb335cc17da5
Brian Faubion <powerinblack@gmail.com>
2021-09-11 11:49:59 -0400

    Added a grid display

--- script/life.js
+++ script/life.js
@@ -1,9 +1,45 @@
 "use strict"
 
-class App extends React.Component {
+// A clickable cell
+function Cell(props) {
+	let class_name = "cell ";
+	if (props.live)
+	{
+		class_name += "live";
+	} else {
+		class_name += "dead";
+	}
+
+	return(
+		<button className={class_name} onClick={ props.onClick }>
+			{props.value}
+		</button>
+	)
+}
+
+class Grid extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.width = 4;
+		this.height = 4;
+	}
+
 	render() {
-		return <h1>Hello world!</h1>
+		let buttons = [];
+		for (let i = 0; i < this.height; ++i)
+		{
+			let row = [];
+			for (let j = 0; j < this.width; ++j)
+			{
+				row.push(<Cell key={i * this.width + j} live={(i + j) % 2} onClick={()=>this.props.onClick()} />);
+			}
+
+			buttons.push(<div key="i" className="grid-row" children={row}/>);
+		}
+
+		return buttons;
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Grid />, document.getElementById('root'));
